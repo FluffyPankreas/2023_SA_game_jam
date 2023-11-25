a9a80a4d519e49545524a6a3d0d816a3026dff02
Eduard Beukes <edpbeukes@gmail.com>
2023-11-25 10:34:34 +0200

    WIP For adding player action functionality.

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -2,34 +2,60 @@ using System.Collections.Generic;
 using DarkMushroomGames.Architecture;
 using PlayerActions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviourSingleton<GameManager>
 {
-    [SerializeField,Tooltip("Reference to the player resources script.")]
+    [SerializeField, Tooltip("Reference to the player resources script.")]
     private Resources playerResources;
-    [SerializeField,Tooltip("Reference to the player attributes script.")]
+
+    [SerializeField, Tooltip("Reference to the player attributes script.")]
     private Attributes playerAttributes;
 
-    [SerializeField,Tooltip("The number of actions that will be selected from all possible actions.")]
+    [SerializeField, Tooltip("The number of actions that will be selected from all possible actions.")]
     private int numberofActionsPerPhase = 3;
-    
+
+    [SerializeField, Tooltip("The button that will be instantiated for player actions.")]
+    private GameObject playerActionButtonPrefab;
+
+    [SerializeField, Tooltip("The parent to hold all the buttons.")]
+    private Transform buttonLayoutParent;
+
     private List<PlayerAction> morningActions;
     private List<PlayerAction> afternoonActions;
     private List<PlayerAction> eveningActions;
+
+    // Building actions should be added
+    // Location actions
+
+    public void Start()
+    {
+        DisplayActions(new List<PlayerAction>());
+    }
+
     public void StartOfDay()
     {
         // Start of day -> Morning action -> Afternoon Action -> Evening Action -> End of Day
     }
-    
+
+    public void PlayerActionClicked()
+    {
+
+    }
+
     public void EndOfDay()
     {
         // Do end of day calculations and see if the player loses.
     }
 
-    
     private void DisplayActions(List<PlayerAction> actionsToShow)
     {
+        Debug.Log("Displaying Actions.");
         //Setup the UI to show the actions
+        var button = Instantiate(playerActionButtonPrefab, buttonLayoutParent);
+        
+        var newAction = new PlayerAction();
+        button.GetComponent<Button>().onClick.AddListener(newAction.CalculateResult);
     }
 
     /// <summary>
@@ -42,14 +68,14 @@ public class GameManager : MonoBehaviourSingleton<GameManager>
             return allActions;
 
         List<PlayerAction> selectedActions = new List<PlayerAction>();
-        
+
         for (int i = 0; i < numberofActionsPerPhase; i++)
         {
             Debug.Log("i: " + i.ToString());
+
             selectedActions = new List<PlayerAction>();
         }
 
         return selectedActions;
     }
-
 }
