6d13ee47dac533b33f0e309be8e9e96c52574e73
Eduard Beukes <edpbeukes@gmail.com>
2023-11-25 07:02:25 +0200

    Blocked out some structures.

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -3,4 +3,30 @@ using DarkMushroomGames.Architecture;
 public class GameManager : MonoBehaviourSingleton<GameManager>
 {
     
+    public void StartOfDay()
+    {
+        
+    }
+
+    public void EndOfDay()
+    {
+        //subtract water/food 
+        //
+        
+    }
+
+    private void SelectMorningActions()
+    {
+        
+    }
+
+    private void SelectAfternoonActions()
+    {
+        
+    }
+
+    private void SelectEveningActions()
+    {
+        
+    }
 }
