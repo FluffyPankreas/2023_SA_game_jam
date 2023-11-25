580efe359ce8e537c5448414a563f0dcfcdcb602
Eduard Beukes <edpbeukes@gmail.com>
2023-11-25 07:21:37 +0200

    Added some references to player stats in the game manager.

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -1,7 +1,18 @@
+using System;
+using System.Collections.Generic;
 using DarkMushroomGames.Architecture;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GameManager : MonoBehaviourSingleton<GameManager>
 {
+    [SerializeField,Tooltip("Reference to the player resources script.")]
+    private Resources playerResources;
+    [SerializeField,Tooltip("Reference to the player attributes script.")]
+    private Attributes playerAttributes;
+    
+    [SerializeField,Tooltip("The list of potential actions the player can take in the morning.")]
+    private List<PlayerAction> morningActions;
     
     public void StartOfDay()
     {
