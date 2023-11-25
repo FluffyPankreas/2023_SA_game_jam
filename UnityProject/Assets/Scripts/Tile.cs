5a012fd7b25ad1fb70a5ae8b0f297f479e37dbab
Eduard Beukes <edpbeukes@gmail.com>
2023-11-25 16:42:56 +0200

    Added stamina cost and generation to the tile.

--- UnityProject/Assets/Scripts/Tile.cs
+++ UnityProject/Assets/Scripts/Tile.cs
@@ -10,15 +10,36 @@ public enum Biomes
 public class Tile
 {
     public Biomes Biome;
+    public int StaminaCost;
 
     public Tile()
     {
         RandomizeBiome();
+        
     }
     
     private void RandomizeBiome()
     {
          Biome = (Biomes)Random.Range(0, 4);
+    }
+
+    private void SetStaminaCost()
+    {
+        switch (Biome)
+        {
+            case Biomes.Desert:
+                StaminaCost = Random.Range(1, 5);
+                break;
+            case Biomes.Forest:
+                StaminaCost = Random.Range(2, 4) ;
+                break;
+            case Biomes.Mountains:
+                StaminaCost = Random.Range(1, 8);
+                break;
+            case Biomes.Plains:
+                StaminaCost = Random.Range(1, 2);
+                break;
+        }
     }
         
     // animals
