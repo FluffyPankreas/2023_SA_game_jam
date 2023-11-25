cdef6e7955a3d5ee91e9c0f73607213cc539b603
Eduard Beukes <edpbeukes@gmail.com>
2023-11-25 17:01:52 +0200

    Added some information to the UI to show the current and next tiles.

--- UnityProject/Assets/Scripts/UIManager.cs
+++ UnityProject/Assets/Scripts/UIManager.cs
@@ -1,20 +1,31 @@
 using DarkMushroomGames.Architecture;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class UIManager : MonoBehaviourSingleton<UIManager>
 {
+    [Header("Player Information")]
     [SerializeField]
     private TextMeshProUGUI foodLabel;
     
     [SerializeField]
-    public TextMeshProUGUI waterLabel;
+    private TextMeshProUGUI waterLabel;
     
     [SerializeField]
-    public TextMeshProUGUI woodLabel;
+    private TextMeshProUGUI woodLabel;
 
+    [Header("Game Information")]
     [SerializeField] 
-    public TextMeshProUGUI debugPhasesLabel;
+    private TextMeshProUGUI phasesLabel;
+
+    [Header("Current Tile")]
+    [SerializeField]
+    private TextMeshProUGUI currentTileBiomeLabel;
+    
+    [Header("Next Tile")]
+    [SerializeField]
+    private TextMeshProUGUI nextTileBiomeLabel;
     
     public void Update()
     {
@@ -22,6 +33,9 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
         waterLabel.text = "Water: " + GameManager.Instance.playerResources.water;
         woodLabel.text = "Wood: " + GameManager.Instance.playerResources.wood;
 
-        debugPhasesLabel.text = GameManager.Instance.currentPhase.ToString();
+        phasesLabel.text = GameManager.Instance.currentPhase.ToString();
+
+        currentTileBiomeLabel.text = "Biome: " + GameManager.Instance.gameProperties.currentTile.Biome;
+        nextTileBiomeLabel.text = "Biome: " + GameManager.Instance.gameProperties.nextTile.Biome;
     }
 }
