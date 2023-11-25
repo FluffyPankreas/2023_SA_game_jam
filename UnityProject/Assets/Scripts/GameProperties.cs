93d209a47079d875245ee153659c9f4ba9f52349
Eduard Beukes <edpbeukes@gmail.com>
2023-11-25 16:37:07 +0200

    Changed the way current tile is tracked.

--- UnityProject/Assets/Scripts/GameProperties.cs
+++ UnityProject/Assets/Scripts/GameProperties.cs
@@ -7,9 +7,9 @@ public class GameProperties : MonoBehaviour
     private List<Tile> _tiles;
 
     public int currentDay;
-    public int babaYagaDistance;// How many tiles the baba yaga is away from the player. 
-    public int currentTileNumber => _tiles.Count;
-    public Tile currentTile => _tiles[^2];
+    public int babaYagaTileIndex; 
+    public int playerTileIndex;
+    public Tile currentTile => _tiles[playerTileIndex];
     public Tile nextTile => _tiles[^1];
     
     public void Start()
