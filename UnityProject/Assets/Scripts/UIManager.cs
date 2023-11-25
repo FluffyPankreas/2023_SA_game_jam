2d9ce25d3da147fcbd89c6a8205a535edc5a4e52
Eduard Beukes <edpbeukes@gmail.com>
2023-11-25 14:13:34 +0200

    Added wood label.

--- UnityProject/Assets/Scripts/UIManager.cs
+++ UnityProject/Assets/Scripts/UIManager.cs
@@ -9,6 +9,9 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
     
     [SerializeField]
     public TextMeshProUGUI waterLabel;
+    
+    [SerializeField]
+    public TextMeshProUGUI woodLabel;
 
     [SerializeField] 
     public TextMeshProUGUI debugPhasesLabel;
@@ -17,6 +20,7 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
     {
         foodLabel.text = "Food: " + GameManager.Instance.playerResources.food;
         waterLabel.text = "Water: " + GameManager.Instance.playerResources.water;
+        woodLabel.text = "Wood: " + GameManager.Instance.playerResources.wood;
 
         debugPhasesLabel.text = GameManager.Instance.currentPhase.ToString();
     }
