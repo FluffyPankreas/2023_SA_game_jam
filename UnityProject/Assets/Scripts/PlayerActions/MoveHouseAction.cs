d897db30d960d191ac7e5f5a04965cca0d5a7fec
Eduard Beukes <edpbeukes@gmail.com>
2023-11-25 11:29:29 +0200

    Added move house action to the project.

---
+++ UnityProject/Assets/Scripts/PlayerActions/MoveHouseAction.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace PlayerActions
+{
+    public class MoveHouseAction : PlayerAction
+    {
+        public override void CalculateResult()
+        {
+            Debug.Log("Move House Action.");
+            // TODO: Move the house to another node in the graph.
+        }
+    }
+}
